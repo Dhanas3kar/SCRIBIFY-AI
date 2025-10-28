c06f751c2fdce0be08e3a9a98f4cc76449ac3e8a
Dharun-prasath <dharunprasath.murugan@gmail.com>
2025-10-28 11:50:05 +0530

    Upload page & MyNotebooks page setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Upload from "./pages/Upload";
+import MyNotebooks from "./pages/MyNotebooks";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/my-notebooks" element={<MyNotebooks />} />
       </Routes>
     </BrowserRouter>
   );
